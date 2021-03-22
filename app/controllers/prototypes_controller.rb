f421f2a64f1d5d1faf130090ec596887b25e27e6
reito010 <79955877+reito010@users.noreply.github.com>
2021-03-22 16:39:50 +0900

    ５回目commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit,:destroy]  
   before_action :set_proto, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index,:show, :destroy,:new]
+  before_action :move_to_index, except: [:index, :show, :new,:create]
   def index
     @prototypes = Prototype.includes(:user)
   end
